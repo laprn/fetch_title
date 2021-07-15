f60405d8a7e4c2fdbb0849dac1b8f23a5226a360
laprn <reg@dymcom.net>
2021-07-15 20:28:22 +0900

    preparation for pypi

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="fetch-info",
-    version="0.1.0",
-    author="y_mrt",
-    author_email="y.mrt@tuta.io",
-    description="fetch a header info from header tag in HTML file.",
+from setuptools import setup
+from codecs import open
+from os import path
+package_name = "fetch_info"
+root_dir = path.abspath(path.dirname(__file__))
+def _requirements():
+    return [name.rstrip() for name in open(path.join(root_dir, 'requirements.txt')).readlines()]
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+setup(
+    name=package_name,
+    version='0.0.2',
+    description='Fetch a header info from header tag in HTML file.',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/laprn/fetch_title.git",
-    packages=setuptools.find_packages(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/laprn/fetch_title.git',
+    author='laprn',
+    author_email='y.mrt@tuta.io',
+    license='MIT',
+    keywords='title',
+    packages=[package_name],
+    install_requires=_requirements(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
-    entry_points = {
-        'console_scripts': ['fetch_title = fetch_info.fetch_header:main']
-    },
-    python_requires='>=3.7',
 )
